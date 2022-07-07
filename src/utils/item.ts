0f6cce6b678b19fd3beb124a87216755ff420cd4
pyphilia <pyphilia@gmail.com>
2022-07-07 12:25:17 +0200

    feat: add buildPathFromId and getIdsFromPath

---
+++ src/utils/item.ts
@@ -0,0 +1,6 @@
+export const buildPathFromId = (id: string): string => {
+  return id.replace(/-/g, '_');
+};
+
+export const getIdsFromPath = (path: string): string[] =>
+  path.replace(/_/g, '-').split('.');
