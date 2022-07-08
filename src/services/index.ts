e6c0e87ec5852538847eabdfcc2b80b6a751a9f3
pyphilia <pyphilia@gmail.com>
2022-07-08 16:26:35 +0200

    refactor: add declare items in fastifyinstance

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,19 @@
+import { ObjectSchema } from 'fluent-json-schema';
+
+import { ItemService, ItemTaskManager } from './items';
+
 export * from './item-memberships';
 export * from './items';
 export * from './members';
 export * from './database';
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    items: {
+      taskManager: ItemTaskManager;
+      dbService: ItemService;
+      extendCreateSchema: (itemTypeSchema?: ObjectSchema) => void;
+      extendExtrasUpdateSchema: (itemTypeSchema?: ObjectSchema) => void;
+    };
+  }
+}
