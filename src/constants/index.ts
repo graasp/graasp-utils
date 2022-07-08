320285c46a446b4a5b9097249f9455a992d4ac8e
pyphilia <pyphilia@gmail.com>
2022-07-08 11:34:06 +0200

    refactor: add http methods enum, item utils

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export * from './contexts';
 export * from './itemTypes';
 export * from './permissionLevels';
 export * from './constants';
+export * from './httpMethods';
--- src/utils/item.test.ts
+++ src/utils/item.test.ts
@@ -1,11 +1,30 @@
-import { buildPathFromId, getIdsFromPath } from './item';
+import { v4 } from 'uuid';
+import {
+  buildPathFromIds,
+  getIdsFromPath,
+  getParentFromPath,
+  getChildFromPath,
+} from './item';
 
 describe('Item Utils', () => {
-  describe('buildPathFromId', () => {
-    it('Build correct paths for values', () => {
+  describe('buildPathFromIds', () => {
+    it('Build correct paths for one id', () => {
       const oneId = 'cc7ad362-c220-4232-a474-decae17371ad';
-      const onePath = buildPathFromId(oneId);
+      const onePath = buildPathFromIds(oneId);
       expect(onePath).toEqual('cc7ad362_c220_4232_a474_decae17371ad');
+    });
+    it('Build correct paths for many ids', () => {
+      const id1 = 'cc7ad362-c220-4232-a474-decae17371ad';
+      const id2 = 'dc7ad362-c220-4232-a474-decae17371ad';
+      const id3 = 'ec7ad362-c220-4232-a474-decae17371ad';
+      const path = buildPathFromIds(id1, id2, id3);
+      expect(path).toEqual(
+        'cc7ad362_c220_4232_a474_decae17371ad.dc7ad362_c220_4232_a474_decae17371ad.ec7ad362_c220_4232_a474_decae17371ad',
+      );
+    });
+    it('Return empty path for empty id', () => {
+      const path = buildPathFromIds();
+      expect(path).toEqual('');
     });
   });
 
@@ -25,6 +44,24 @@ describe('Item Utils', () => {
         'cc7ad362-c220-4232-a474-decae17371a3',
         'cc7ad362-c220-4232-a474-decae17371a2',
       ]);
+    });
+  });
+
+  describe('getParentFromPath', () => {
+    test('get parent id', () => {
+      const parent = v4();
+      const child = v4();
+      expect(getParentFromPath(buildPathFromIds(parent, child))).toBe(parent);
+      expect(getParentFromPath(buildPathFromIds(child))).toBe(undefined);
+    });
+  });
+
+  describe('getChildFromPath', () => {
+    test('extract child id', () => {
+      const parent = v4();
+      const child = v4();
+      expect(getChildFromPath(buildPathFromIds(parent, child))).toBe(child);
+      expect(getChildFromPath(buildPathFromIds(child))).toBe(child);
     });
   });
 });
