477b9bc98e7de68bf7739b68aef33a2d5dfd441a
pyphilia <pyphilia@gmail.com>
2022-07-08 11:01:13 +0200

    refactor: rename graasp error file

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,7 @@
-export type { Actor } from './actor';
+export * from './actor';
+export * from './base-task';
 export * from './extra';
+export * from './graasp-error';
 export * from './requests';
 export * from './task-runner';
 export * from './task';
-export * from './base-task';
-export * from './graasp-error';
